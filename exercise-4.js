4c29c5e0e055600db644398d3a111ff413a0ce21
FajarRhamdhani <rhamdhani.fajar3@gmail.com>
2019-10-22 21:55:59 +0700

    update baru

--- exercise-4.js
+++ exercise-4.js
@@ -1,43 +1,43 @@
-var tanggal       = "28" ;
-var bulan         = "2" ;
-var tahun         = "2013" ;
+var tanggal  = 31 ;
+var bulan    = 13 ;
+var tahun    = 1900 ;
 
 
 switch(bulan) {
-    case "1":
+    case 1:
         bulan = "Januari";
         break;
-    case "2":
+    case 2:
         bulan = "Februari";
         break;
-    case "3":
+    case 3:
         bulan = "Maret";
         break;
-    case "4":
+    case 4:
         bulan = "April";
         break;
-    case "5":
+    case 5:
         bulan = "Mei";
         break;
-    case "6":
+    case 6:
         bulan = "Juni";
         break;
-    case "7":
+    case 7:
         bulan = "Juli";
         break;
-    case "8":
+    case 8:
         bulan = "Agustus";
         break;
-    case "9":
+    case 9:
         bulan = "September";
         break;
-    case "10":
+    case 10:
         bulan = "Oktober";
         break;
-    case "11":
+    case 11:
         bulan = "November";
         break;
-    case "12":
+    case 12:
         bulan = "Desember";
         break;
     default:
@@ -45,24 +45,24 @@ switch(bulan) {
         break;
 } 
 
-var formatTanggal = tanggal + " " + bulan + " " + tahun; 
-if((tahun % 4 === 0) && (tahun % 100 !== 0 || tahun % 100 === 0 || tahun % 400 === 0) && (tanggal >="1") && (tanggal <= "29") && (bulan === "Februari") && (tahun >= "1900") && (tahun <= "2200")){
+var formatTanggal =`${tanggal} ${bulan} ${tahun}`; 
+if((tahun % 4 === 0) && (tahun % 100 !== 0 || tahun % 100 === 0 || tahun % 400 === 0) && (tanggal >=1) && (tanggal <= 29) && (bulan === "Februari") && (tahun >= 1900) && (tahun <= 2200)){
     console.log(formatTanggal);
 }
-else if((tanggal >="1") && (tanggal <= "28") && (bulan === "Februari") && (tahun >= "1900") && (tahun <= "2200")) {
+else if((tanggal >=1) && (tanggal <= 28) && (bulan === "Februari") && (tahun >= 1900) && (tahun <= 2200)) {
     console.log(formatTanggal);
 }
-else if ((tanggal >="1") && (tanggal <= "30") && (bulan === "April" || bulan === "Juni" || bulan === "September" || bulan === "November") && (tahun >= "1900") && (tahun <= "2200"))
+else if ((tanggal >=1) && (tanggal <= 30) && (bulan === "April" || bulan === "Juni" || bulan === "September" || bulan === "November") && (tahun >= "1900") && (tahun <= "2200"))
 {
     console.log(formatTanggal);
 }
-else if ((tanggal >="1") && (tanggal <= "31") && (bulan === "Januari" ||bulan === "Maret" || bulan === "Mei" || bulan === "Juli" || bulan === "Agustus" || bulan=== "Oktober" || bulan === "Desember") && (tahun >= "1900") && (tahun <= "2200"))
+else if ((tanggal >=1) && (tanggal <= 31) && (bulan === "Januari" ||bulan === "Maret" || bulan === "Mei" || bulan === "Juli" || bulan === "Agustus" || bulan=== "Oktober" || bulan === "Desember") && (tahun >= 1900) && (tahun <= 2200))
 {
     console.log(formatTanggal);
 }
 else
 {
-    console.log("Cek Kembali Tanggal/Bulan/ Tahun yang dimasukkan");
+    console.log("Cek Kembali Tanggal / Bulan / Tahun yang dimasukkan");
 }
 
 
